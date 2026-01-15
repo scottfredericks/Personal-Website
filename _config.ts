d45abbcbf1e45eb03238ec9b87b7c3662d47c5f6
Scott Fredericks <swf767@gmail.com>
2026-01-15 11:43:47 -0800

    Add minifier plugins; add css, js, and svg by extension

--- _config.ts
+++ _config.ts
@@ -1,5 +1,9 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts"; // Used by blog list
+import esbuild from "lume/plugins/esbuild.ts"; // Minify and bundle JS and TS
+import lightningCss from "lume/plugins/lightningcss.ts"; // Minify CSS
+import minifyHTML from "lume/plugins/minify_html.ts"; // Minify HTML
+import svgo from "lume/plugins/svgo.ts"; // Minify SVG
 
 const site = lume({
   src: "./src",
@@ -13,10 +17,20 @@ const site = lume({
   },
 });
 
-site.add("/css");
-site.add("/js");
+site.add([".css"]);
+site.add([".js"]);
+site.add([".svg"]);
 site.add("/img");
 
 site.use(date());
+site.use(esbuild());
+site.use(lightningCss());
+site.use(minifyHTML({
+  options: {
+    minify_css: true,
+    minify_js: true,
+  },
+}));
+site.use(svgo());
 
 export default site;
